36075ae6815be25843420536fdd18ff3aa3e7cd4
Puspita Vabna <sharif7761@gmail.com>
2022-02-17 01:29:03 +0600

    solved team problem

---
+++ codeforces/231A - Team.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c, x, count = 0;
+    cin >> x;
+    for(int i = 0; i < x; i++){
+        cin >> a >> b >> c;
+        if((a + b + c) >= 2) {
+            count++;
+        }
+    }
+    cout << count << endl;
+
+    return 0;
+}
