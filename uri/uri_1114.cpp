b7a313a490dd4426fa9e406f9a31aeb1eb64d6c7
Puspita Vabna <sharif7761@gmail.com>
2022-03-05 23:08:15 +0600

    solved uri 1114

---
+++ uri/uri_1114.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+ 
+int main() {
+    int password;
+        while(true){
+            cin >> password;
+            if(password == 2002) {
+                cout << "Acesso Permitido"<< endl;
+                break;
+            }
+            cout << "Senha Invalida"<<endl;     
+        }
+    return 0;
+}
