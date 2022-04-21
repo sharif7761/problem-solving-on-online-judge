3b18a531a567b0f6b2985f1ef4cfdff2dc747124
Puspita Vabna <sharif7761@gmail.com>
2022-04-21 23:19:30 +0600

    solved uri 1184

---
+++ uri/uri_1184.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+   double arr[12][12], sum = 0;
+   char operation;
+   cin >> operation;
+   for(int i = 0; i < 12; i++){
+       for(int j = 0; j < 12; j++){
+           cin >> arr[i][j];
+       }
+   }
+   
+   for(int k = 1; k < 12; k++){
+       for(int l = 0; l < k; l++){
+            sum += arr[k][l];
+       }
+       
+   }
+   if(operation == 'M'){
+        sum /= 66.0;       
+    }
+    cout << fixed << setprecision(1) << sum <<endl;
+
+    return 0;
+}
