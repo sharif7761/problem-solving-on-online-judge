f75977d9ecc8ccb46c06d2c52794b4bfdce9811c
ahmed7761 <sharif7761@gmail.com>
2022-08-05 20:12:06 +0600

    check whether a number is palindrome

---
+++ weekly-task/palindrome.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int checkLength(string givenString){
+    int length = 0;
+    for(int i=0; givenString[i]!='\0'; i++)
+    {
+        length++;
+    }
+    return length;
+}
+
+int main()
+{
+    string inputString;
+    cin >> inputString;
+    
+    // int inputLeng = inputString.length();
+    int inputLeng = checkLength(inputString);
+    
+    for(int i=0; i < inputLeng / 2; i++, inputLeng--){
+        if(inputString[i] != inputString[inputLeng-1]){
+            cout << "Not palindrome";
+            return 0;
+        }
+    }
+        cout << "Palindrome";
+            
+    return 0;
+}
