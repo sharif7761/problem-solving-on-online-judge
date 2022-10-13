d7dea921477ed87eb94b58096771b804156a85c4
ahmed7761 <sharif7761@gmail.com>
2022-10-14 00:11:37 +0600

    updated routes pair problem

--- weekly-task/routes pair problem.js
+++ weekly-task/routes pair problem.js
@@ -1,10 +1,12 @@
 function routePairs(arr, n)
 {
+    let routes = []
     for (var i = 0; i < n -1; i++) {
         for (var j = i+1; j < n; j++) {
-            document.write(arr[i] + " - " + arr[j]);
+            routes.push(arr[i] + " - " + arr[j]);
         }
     }
+    console.log(routes)
 }
  
 var arr = [ 'Dhaka', 'CTG', 'COXS', 'Bandarban' ];
