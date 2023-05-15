c49dcfc3b91a822d634e7fbc676d8f03b42c7d69
ahmed7761 <sharif7761@gmail.com>
2023-05-15 23:35:34 +0600

    solved leetcode 1688. Count of Matches in Tournament problem

---
+++ leet_code/1688. Count of Matches in Tournament.js
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/count-of-matches-in-tournament/
+
+var numberOfMatches = function(n) {
+    let matches = 0;
+    while(n > 1){
+        matches += Math.floor(n/2)
+        n = Math.ceil(n/2)
+    }
+    return matches;
+};
